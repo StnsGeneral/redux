bcc6ca35a4d21ebbe1896bc6cc766f85809b0e96
StnsGeneral <tristanmiller89@gmail.com>
2021-06-16 21:22:23 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/FavoriteMovieList.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { removeFavorite } from '../actions/favoriteActions';
+
+const FavoriteMovieList = (props) => {
+  return (
+    <div className="col-xs savedContainer">
+      <h5>Favorite Movies</h5>
+      {props.favorites.map((movie) => {
+        return (
+          <div key={movie.id}>
+            <Link
+              className="btn btn-light savedButton"
+              to={`/movies/${movie.id}`}>
+              {movie.title}
+              <span>
+                <span
+                  className="material-icons"
+                  onClick={() => props.removeFavorite(movie.id)}>
+                  remove_circle
+                </span>
+              </span>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    favorites: state.favorites.favorites,
+  };
+};
+
+export default connect(mapStateToProps, { removeFavorite })(FavoriteMovieList);
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import favoritesReducer from './favoritesReducer';
+import movieReducer from './movieReducer';
+
+export default combineReducers({
+  movies: movieReducer,
+  favorites: favoritesReducer,
+});
